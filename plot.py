a58245d2811addc62d141b512c7d564ec9be249a
Steve Rowe <simuloid@gmail.com>
2020-04-03 21:17:56 -0400

    Added plot.py

---
+++ plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#if __name__ == '__main__':
+x = np.linspace(0, 6.29, 1000)
+plt.plot(x, np.sin(x))
+plt.title('About as simple as it gets, folks')
+plt.show()
+
+
